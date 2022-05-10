2d08f9ed7cd8b25de26711f479962a94da1dfe15
Christian Weyer <christian.weyer@thinktecture.com>
2022-05-10 16:21:29 +0200

    Adding authN to Blazor client (does not yet work -> we need to talk about HTTPS now…)

--- src/OrderMonitorClient/Program.cs
+++ src/OrderMonitorClient/Program.cs
@@ -3,17 +3,27 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OrderMonitorClient;
 using MudBlazor.Services;
 using OrderMonitorClient.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress =
-    new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("OrderMonitor", c =>
+    c.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+builder.Services.AddScoped(services => services.GetRequiredService<IHttpClientFactory>()
+    .CreateClient("OrderMonitor"));
 
 builder.Services.AddScoped<OrderMonitorService>();
 
 builder.Services.AddMudServices();
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Oidc", options.ProviderOptions);
+});
 
 await builder.Build().RunAsync();
